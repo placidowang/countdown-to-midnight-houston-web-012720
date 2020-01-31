bca9950178cf6b35600735cdcec36ead327bc48f
Placido Wang <placidow@gmail.com>
2020-01-31 22:10:45 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
+def countdown(num)
+  i = num
+  while i > 0 do
+    puts "#{i} SECOND(S)!"
+    i -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(num)
+  i = num
+  while i > 0 do
+    puts "#{i} SECOND(S)!"
+    sleep(1)
+    i -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+# countdown_with_sleep(2)
